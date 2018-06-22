60ae25f77b8d6d5d506acd6fe272814c9fe8892a
Julie <julielerman@gmail.com>
2018-06-22 10:54:27 -0400

    new method to show changes to batch command in 2.1

--- M3Downloads/After/SamuraiApp/SomeUI/Program.cs
+++ M3Downloads/After/SamuraiApp/SomeUI/Program.cs
@@ -15,6 +15,7 @@ namespace SomeUI
         {
             //InsertSamurai();
             //InsertMultipleSamurais();
+            InsertMultipleSamuraisViaBatch();
             //SimpleSamuraiQuery();
             //MoreQueries();
             //RetrieveAndUpdateSamurai();
@@ -28,6 +29,21 @@ namespace SomeUI
             //DeleteMany();
             //DeleteWhileNotTracked();
             //DeleteUsingId(3);
+
+        }
+        private static void InsertMultipleSamuraisViaBatch()
+        {
+            //EF Core 2.1 added a minimum batch size which defaults to 4
+            //this 4 object insert WILL be batched
+            var samurai1 = new Samurai { Name = "Samurai1" };
+            var samurai2 = new Samurai { Name = "Samurai2" };
+            var samurai3 = new Samurai { Name = "Samurai3" };
+            var samurai4 = new Samurai { Name = "Samurai4" };
+            using (var context = new SamuraiContext())
+            {
+                context.Samurais.AddRange(samurai1, samurai2, samurai3, samurai4);
+                context.SaveChanges();
+            }
         }
 
         private static void DeleteUsingId(int samuraiId)
